a41370385dd27f1c7604c22428fda7564ecb1b78
Nick Pisciotta <npisciotta@gmail.com>
2016-08-29 11:42:58 -0600

    Add index file and ability to post new data

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.set('view engine', 'jade');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const generateId = require('./lib/generate-id');
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Pizza Express';
+
+app.get('/', (request, response) => {
+  response.render("index");
+});
+
+app.post('/pizzas', (request, response) => {
+  if (!request.body.pizza) { return response.sendStatus(400); }
+  var id = generateId();
+  app.locals.pizzas[id] = request.body;
+  response.redirect('/pizzas/' + id);
+});
+
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id];
+  response.render('pizza', { pizza: pizza });
+});
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+app.locals.pizzas = {};
+
+module.exports = app;
